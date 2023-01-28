ffd02b57c4bff02dcc3fd725ea220f9d10548bb1
Juan Diego <jdoliveros@gtsolutions.io>
2023-01-28 10:45:42 -0500

    useState example

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,14 +1,21 @@
 import PropTypes from "prop-types";
 
-export const FirstApp = ({title,subtitle}) => {
+export const FirstApp = ({title, subtitle,name}) => {
     return (
         <>
             <h1>{title}</h1>
             <p>{subtitle}</p>
+            <p>{name}</p>
         </>
     );
 }
 FirstApp.propTypes = {
-    title:PropTypes.string.isRequired,
-    subtitle:PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.number.isRequired,
+}
+
+FirstApp.defaultProps = {
+    title: "This is a default title",
+    subtitle: 2023,
+    name: "Juan Diego oliveros",
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from 'react-dom/client'
 import {FirstApp} from "./FirstApp.jsx";
 import "./styles.css"
+import CounterApp from "./CounterApp.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title={"My Dude"} subtitle={123}/>
+        <CounterApp value={1}/>
     </React.StrictMode>
 )
